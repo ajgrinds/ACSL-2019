2294f6f76e48e0d829e464265cd9822485eec495
ajgrinds <ajgrinds03@gmail.com>
2019-12-17 20:36:39 -0800

    1 line

--- round1.py
+++ round1.py
@@ -1,7 +1,2 @@
-for n, p in map(lambda x: [list(map(lambda a: int(a), z)) if i == 0 else int(z) for i, z in enumerate(filter(lambda y: y, x.split(" ")))], open("round1Input.txt").read().splitlines()):
-    # lol
-    for index in range(len(n) - p + 1, len(n)):
-        n[index] = abs(n[index] - n[len(n) - p])
-    for index in range(len(n) - p):
-        n[index] = (n[index] + n[len(n) - p]) % 10
-    print(*n, sep="")
+[print(*[*[(n[index] + n[len(n) - p]) % 10 for index in range(len(n) - p)], n[len(n) - p], *[abs(n[index] - n[len(n) - p]) for index in range(len(n) - p + 1, len(n))]], sep="") for n, p in map(lambda x: [list(map(lambda a: int(a), z)) if i == 0 else int(z) for i, z in enumerate(filter(lambda y: y, x.split(" ")))], open("round1Input.txt").read().splitlines())]
+# lol
