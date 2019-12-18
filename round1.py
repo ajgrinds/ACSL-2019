9117a5c0f58edc2220a8d86f19433520fc28f60a
ajgrinds <ajgrinds03@gmail.com>
2019-12-17 20:28:13 -0800

    Bug fixes:

--- round1.py
+++ round1.py
@@ -1,7 +1,7 @@
 for n, p in map(lambda x: [list(map(lambda a: int(a), z)) if i == 0 else int(z) for i, z in enumerate(filter(lambda y: y, x.split(" ")))], open("round1Input.txt").read().splitlines()):
     # lol
-    for index in range(p + 1, len(n)):
+    for index in range(len(n) - p + 1, len(n)):
         n[index] = abs(n[index] - n[len(n) - p])
-    for index in range(p):
+    for index in range(len(n) - p):
         n[index] = (n[index] + n[len(n) - p]) % 10
     print(*n, sep="")
