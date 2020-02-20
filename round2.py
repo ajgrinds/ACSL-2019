d2dc02f1ab60bdb658af8ea557acede82b606a77
ajgrinds <ajgrinds03@gmail.com>
2020-02-20 12:28:22 -0800

    There was a little more logic so I couldn't do 1 line but I guess this is something

--- round2.py
+++ round2.py
@@ -1,32 +1,20 @@
-io = [(list(y[0]), list(y[1])) for y in (x.split(" ") for x in open("round2Input.txt").read().splitlines())]
-
-for z in io:
+for a, z in enumerate([(list(y[0]), list(y[1])) for y in (x.split(" ") for x in open("round2Input.txt").read().splitlines())]):
     while True:
         i = 0
         while i < min(len(z[0]), len(z[1])):
             if z[0][i] == z[1][i]:
-                del z[0][i]
-                del z[1][i]
+                del z[0][i], z[1][i]
             else:
                 i += 1
 
-        i = 0
-        while i < min(len(z[0]), len(z[1])):
+        for i in range(min(len(z[0]), len(z[1]))):
             if z[1][i] in z[0] and z[0].index(z[1][i]) == i + 1:
                 z[0].pop(i)
                 break
             elif z[0][i] in z[1] and z[1].index(z[0][i]) == i + 1:
                 z[1].pop(i)
                 break
-            i += 1
         else:
             break
 
-i = 0
-for a, b in io:
-    i += 1
-    score = 0
-    for x in zip(a, b):
-        score += ord(x[0]) - ord(x[1])
-    score += max(len(a), len(b)) - min(len(a), len(b))
-    print(f"{i}. {score}")
+    print(f"{a + 1}. {sum(ord(x[0]) - ord(x[1]) for x in zip(z[0], z[1])) + max(len(z[0]), len(z[1])) - min(len(z[0]), len(z[1]))}")
